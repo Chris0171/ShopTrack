3296cfe25265c675c86e6d4a9ba90fece5405ea4
Christian Milanes <christianmilanes01@gmail.com>
2025-11-18 02:25:55 +0100

    initial logic created

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hola mundo');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindows() {
+	const mainWindows = new BrowserWindow({
+		width: 1020,
+		height: 680,
+		minWidth: 400,
+		minHeight: 400,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'), // opcional
+			// nodeIntegration: true, // si quieres usar Node.js en tu ventana
+			// contextIsolation: false,
+		},
+	})
+	mainWindows.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+	createWindows()
+})
