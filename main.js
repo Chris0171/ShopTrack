9419de9a3ba259c8b71b889c19bde9be362ba34b
Christian Milanes <christianmilanes01@gmail.com>
2025-11-19 05:47:44 +0100

    preload and views configured

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const fs = require('fs')
 const path = require('path')
 
 function createWindows() {
@@ -8,14 +9,29 @@ function createWindows() {
 		minWidth: 400,
 		minHeight: 400,
 		webPreferences: {
-			//preload: path.join(__dirname, 'renderer.js'), // opcional
+			preload: path.join(__dirname, 'preload.js'),
 			// nodeIntegration: true, // si quieres usar Node.js en tu ventana
-			contextIsolation: false,
+			contextIsolation: true,
 		},
 	})
 	mainWindows.loadFile('index.html')
 }
 
+Menu.setApplicationMenu(null)
+
+ipcMain.handle('load-view', (event, viewName) => {
+	const viewPath = path.join(__dirname, 'views', viewName)
+	return fs.readFileSync(viewPath, 'utf8')
+})
+
 app.whenReady().then(() => {
 	createWindows()
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindows()
+	})
+})
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit()
 })
