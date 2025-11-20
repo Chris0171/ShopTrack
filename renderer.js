ca5b59466bc679d97f096fe6aa96fd7a77ea115d
Christian Milanes <christianmilanes01@gmail.com>
2025-11-20 23:42:53 +0100

    new functions called

--- renderer.js
+++ renderer.js
@@ -1,14 +1,38 @@
 window.addEventListener('DOMContentLoaded', async () => {
 	const content = document.getElementById('mainContent')
 
-	// cargar vista inicial
-	content.innerHTML = await window.api.loadView('dashboard.html')
+	// Cargar vista inicial
+	await loadView('dashboard.html')
 
-	// Delegación de evento para cualquier <data-view="">
+	// Delegación de clics para botones/menu
 	document.querySelectorAll('[data-view]').forEach((item) => {
 		item.addEventListener('click', async () => {
 			const view = item.getAttribute('data-view')
-			content.innerHTML = await window.api.loadView(view)
+			await loadView(view)
 		})
 	})
 })
+
+async function loadView(viewName) {
+	const content = document.getElementById('mainContent')
+
+	content.innerHTML = await window.api.loadView(viewName)
+
+	// Cargar JS asociado a la vista
+	loadViewScript(viewName)
+}
+
+function loadViewScript(viewName) {
+	// Extrae nombre sin extensión => nuevaVenta
+	const baseName = viewName.split('.')[0]
+
+	const scriptPath = `./assets/js/${baseName}.js`
+
+	// Cargar dinámicamente el script
+	const script = document.createElement('script')
+	script.src = scriptPath
+	script.type = 'module'
+	document.body.appendChild(script)
+
+	console.log('→ Script cargado:', scriptPath)
+}
