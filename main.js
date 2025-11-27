78bc298c2968326eefa02725fbe2242e9bebffd8
Christian Milanes <christianmilanes01@gmail.com>
2025-11-27 02:29:37 +0100

    venta-controller ipc functions implemented

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path')
 // ** === Controladores === ** //
 const productoController = require('./backend/controllers/producto-controller')
 const clienteController = require('./backend/controllers/cliente-controller')
+const ventaController = require('./backend/controllers/venta-controller')
 
 const db = require('./backend/db/initDatabase') // * Inicializa DB
 
@@ -137,6 +138,56 @@ ipcMain.handle('cliente:getById', async (event, id) => {
 		clienteController.getById(id, (err, row) => {
 			if (err) resolve({ ok: false, error: err.message })
 			else resolve({ ok: true, cliente: row })
+		})
+	})
+})
+
+// ** === Venta == ** //
+ipcMain.handle('venta:create', async (event, data) => {
+	return new Promise((resolve) => {
+		ventaController.create(data, (err, result) => {
+			if (err) resolve({ ok: false, error: err.message })
+			else resolve({ ok: true, id: result.id })
+		})
+	})
+})
+ipcMain.handle('venta:getAll', async () => {
+	return new Promise((resolve) => {
+		ventaController.getAll((err, rows) => {
+			if (err) resolve({ ok: false, error: err.message })
+			else resolve({ ok: true, ventas: rows })
+		})
+	})
+})
+ipcMain.handle('venta:update', async (event, { id, data }) => {
+	return new Promise((resolve) => {
+		ventaController.update(id, data, (err, result) => {
+			if (err) resolve({ ok: false, error: err.message })
+			else resolve({ ok: true, changes: result.changes })
+		})
+	})
+})
+ipcMain.handle('venta:delete', async (event, id) => {
+	return new Promise((resolve) => {
+		ventaController.delete(id, (err, result) => {
+			if (err) resolve({ ok: false, error: err.message })
+			else resolve({ ok: true, changes: result.changes })
+		})
+	})
+})
+ipcMain.handle('venta:getById', async (event, id) => {
+	return new Promise((resolve) => {
+		ventaController.getById(id, (err, row) => {
+			if (err) resolve({ ok: false, error: err.message })
+			else resolve({ ok: true, venta: row })
+		})
+	})
+})
+ipcMain.handle('venta:getByClienteId', async (event, idCliente) => {
+	return new Promise((resolve) => {
+		ventaController.getByClienteId(idCliente, (err, rows) => {
+			if (err) resolve({ ok: false, error: err.message })
+			else resolve({ ok: true, ventas: rows })
 		})
 	})
 })
