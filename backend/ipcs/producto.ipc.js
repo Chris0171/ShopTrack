7b84234a3f16ca565027cd24d0748749f220b237
Christian Milanes <christianmilanes01@gmail.com>
2025-12-02 09:03:05 +0100

    registerProductoIPC function created

---
+++ backend/ipcs/producto.ipc.js
@@ -0,0 +1,47 @@
+const productoController = require('../controllers/producto-controller.js')
+
+module.exports = function registerProductoIPC(ipcMain) {
+	ipcMain.handle('producto:getAll', async () => {
+		return new Promise((resolve, reject) => {
+			productoController.getAll((err, rows) => {
+				if (err) return reject(err.message)
+				resolve(rows)
+			})
+		})
+	})
+
+	ipcMain.handle('producto:create', async (event, data) => {
+		return new Promise((resolve, reject) => {
+			productoController.create(data, (err, result) => {
+				if (err) reject(err.message)
+				else resolve(result)
+			})
+		})
+	})
+	ipcMain.handle('producto:buscar-producto', async (event, nroParte) => {
+		return new Promise((resolve, reject) => {
+			productoController.buscarPorNroParte(nroParte, (err, row) => {
+				if (err) return reject(err.message)
+				resolve(row)
+			})
+		})
+	})
+	ipcMain.handle(
+		'producto:actualizar-stock',
+		async (event, idProducto, nuevaCantidad) => {
+			return new Promise((resolve) => {
+				productoController.actualizarStock(
+					idProducto,
+					nuevaCantidad,
+					(err, result) => {
+						if (err) {
+							resolve({ ok: false, error: err.message })
+						} else {
+							resolve({ ok: true })
+						}
+					}
+				)
+			})
+		}
+	)
+}
