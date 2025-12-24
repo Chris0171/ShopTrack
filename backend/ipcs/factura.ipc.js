9124fa823a59fb003e6e23c24325c15f3497e8a1
Christian Milanes <christianmilanes01@gmail.com>
2025-12-24 17:43:22 +0100

    generarPDF function created

--- backend/ipcs/factura.ipc.js
+++ backend/ipcs/factura.ipc.js
@@ -1,4 +1,6 @@
 const facturaController = require('../controllers/factura-controller')
+const PDFService = require('../services/pdf-service')
+const detalleVentaController = require('../controllers/detalleVenta-controller')
 
 module.exports = function registerFacturaIPC(ipcMain) {
 	ipcMain.handle('factura:create', async (event, data) => {
@@ -49,4 +51,28 @@ module.exports = function registerFacturaIPC(ipcMain) {
 			})
 		})
 	})
+	ipcMain.handle(
+		'factura:generarPDF',
+		async (event, { factura, cliente, detalles }) => {
+			try {
+				const datosFactura = {
+					numeroFactura: factura.numeroFactura,
+					fechaEmision: factura.fechaEmision,
+					cliente: cliente,
+					detalles: detalles,
+					subtotal: factura.subtotal,
+					impuestos: factura.impuestos,
+					descuento: factura.descuento || 0,
+					total: factura.total,
+					metodoPago: factura.metodoPago,
+					observaciones: factura.observaciones,
+				}
+
+				const resultado = await PDFService.generarFacturaPDF(datosFactura)
+				return { ok: true, ...resultado }
+			} catch (error) {
+				return { ok: false, error: error.message }
+			}
+		}
+	)
 }
