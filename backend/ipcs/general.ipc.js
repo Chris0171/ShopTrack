08322638c8408d0015d2ed86c775d845446b9b1e
Christian Milanes <christianmilanes01@gmail.com>
2025-12-24 17:44:45 +0100

    abrirArchivo function created

--- backend/ipcs/general.ipc.js
+++ backend/ipcs/general.ipc.js
@@ -1,9 +1,19 @@
 const path = require('path')
 const fs = require('fs')
+const { shell } = require('electron')
 
 module.exports = function registerGeneralIPC(ipcMain) {
 	ipcMain.handle('load-view', (event, viewName) => {
 		const viewPath = path.join(__dirname, '../../views', viewName)
 		return fs.readFileSync(viewPath, 'utf8')
+	})
+
+	ipcMain.handle('abrirArchivo', async (event, ruta) => {
+		try {
+			await shell.openPath(ruta)
+			return { ok: true }
+		} catch (error) {
+			return { ok: false, error: error.message }
+		}
 	})
 }
